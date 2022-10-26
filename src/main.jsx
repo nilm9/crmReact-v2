37b31451f0e785050f90b53bec0e3d35a14842d2
Nil Monfort <nil.monfort@estudiants.urv.cat>
2022-10-26 12:00:13 +0200

    routing n more

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Layout from './components/Layout'
+import NewCustomer from './pages/NewCustomer'
+import Index, {loader as customersLoader} from './pages/Index'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        //Inherits and adds w index
+        index: true,
+        element: <Index />,
+        loader: customersLoader
+      },
+      {
+        path: '/customers/new',
+        element: <NewCustomer/>
+      },
+    ]
+  },
+
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
