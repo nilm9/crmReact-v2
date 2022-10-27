42aec6e5dfa28c76ea1b19dea862eaab7ef933e6
Nil Monfort <nil.monfort@estudiants.urv.cat>
2022-10-27 10:23:55 +0200

    newclient

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom"
+import Client from "../components/Client";
 
 export function loader() {
     const clientes = [
@@ -7,50 +8,72 @@ export function loader() {
             nombre: 'Juan',
             telefono: 102013313,
             email: "juan@juan.com",
-            empresa: 'Codigo Con Juan'
+            empresa: 'Microsoft'
         },
         {
             id: 2,
             nombre: 'Karen',
             telefono: 138198313,
             email: "karen@juan.com",
-            empresa: 'Codigo Con Juan'
+            empresa: 'Microsoft'
         },
         {
             id: 3,
             nombre: 'Josue',
             telefono: 31983913,
             email: "josue@juan.com",
-            empresa: 'Codigo Con Juan'
+            empresa: 'Microsoft'
         },
         {
             id: 4,
             nombre: 'Miguel',
             telefono: 319381983,
             email: "miguel@juan.com",
-            empresa: 'Codigo Con Juan'
+            empresa: 'Microsoft'
         },
         {
             id: 5,
             nombre: 'Pedro',
             telefono: 1398198938,
             email: "pedro@juan.com",
-            empresa: 'Codigo Con Juan'
+            empresa: 'Microsoft'
         },
     ];
     return clientes
 }
 const Index = () => {
 
-    const data = useLoaderData();
-    console.log(data);
+    const clients = useLoaderData();
   return (
     <>
-    <h1 className='font-black text-4xl text-blue-900'> Customers</h1>
+    <h1 className='font-black text-4xl text-blue-900'> Clients</h1>
 
-    <p className="mt-3"> Manage your customers</p>
+    <p className="mt-3"> Manage your clients</p>
     
-    
+    {clients.length ? (
+        <table className="w-full bg-white mt-5 shadow table-autos">
+            <thead className="bg-blue-900 text-white">
+                <tr>
+                    <th className="p-2">Client</th>
+                    <th className="p-2">Contact</th>
+                    <th className="p-2">Actions</th>
+
+                </tr>
+            </thead>
+
+                <tbody>
+                    {clients.map( client => (
+                        <Client
+                        client={client}
+                        key={client.id}
+                        />
+                         )
+                    )}
+                </tbody>
+        </table>
+    ) : (
+        <p className="text-center mt-10">No customers yet </p>
+    )}
     
     </>
   )
--- src/pages/NewCustomer.jsx
+++ src/pages/NewCustomer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const NewCustomer = () => {
   return (
-    <div>NewCustomer</div>
+    <>
+        <h1 className='font-black text-4xl text-blue-900'>New Client</h1>
+        <p className="mt-3"> fill all the fields to add a new customer</p>
+    
+    
+    </>
   )
 }
 
